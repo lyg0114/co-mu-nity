06737d5136ea8ebf60427e52338c828d2bbdd0f0
lyg0114 <whdnseowkd@naver.com>
2024-10-19 21:52:50 +0900

    add : BaseeDto 추가

---
+++ src/main/java/com/comu/comunity/model/dto/BaseDto.java
@@ -0,0 +1,19 @@
+package com.comu.comunity.model.dto;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.comu.comunity.model.dto
+ * @since : 19.10.24
+ */
+@Getter
+@SuperBuilder
+public class BaseDto {
+
+	private String email;
+
+	private String name;
+
+}
