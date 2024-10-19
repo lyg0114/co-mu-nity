e9a94fbc2dffb1cd07ef0a23889ba2501fc2b7ff
gebeen-dev <rrdd1207@gmail.com>
2024-10-19 17:18:47 +0900

    update : CommentEntity 추가

--- src/main/java/com/comu/comunity/model/entity/Comment.java
+++ src/main/java/com/comu/comunity/model/entity/Comment.java
@@ -1,4 +1,22 @@
 package com.comu.comunity.model.entity;
 
-public class Comment extends BaseEntity{
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "comment")
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String contents;
 }
