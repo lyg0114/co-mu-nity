de3ef560ad7f03a8438d5d88d048f1d27247a52e
Jisoo.R <jisuryu0527@gmail.com>
2024-10-19 15:05:19 +0900

    Update: MemberRepository

--- src/main/java/com/comu/comunity/repository/MemberRepository.java
+++ src/main/java/com/comu/comunity/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package com.comu.comunity.repository;
 
-public class MemberRepository {
+import com.comu.comunity.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
